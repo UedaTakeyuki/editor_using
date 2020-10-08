9b929e3cead66d25e6aa61f0b82e4e03040d2aac
Takeyuki UEDA <ueda@latelierdueda.com>
2020-10-08 10:47:58 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <v-main>
+      <router-view/>
+    </v-main>
   </div>
 </template>
 
@@ -30,3 +28,16 @@
   color: #42b983;
 }
 </style>
+
+<script>
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
